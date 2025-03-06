b425ca36bd202f757b0d37e643c9550654aee0c6
Gyula Madarasz <gyula.madarasz@pm.me>
2025-03-06 12:01:09 +0000

    ctags functions only

--- ctags.sh
+++ ctags.sh
@@ -1,1 +1,1 @@
-ctags --output-format=json -R src/tools | grep -v '"name": "test_' > ctags.json
+ctags --output-format=json -R src/tools | grep -Ev '"(name|scope)": "test_' | grep '"kind": "function"' > ctags.json
