31ea0a8ed2a6d968f50b0d81306d5f4a984b1594
Gyula Madarasz <gyula.madarasz@pm.me>
2025-03-23 23:24:40 +0000

    cleanup

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# echo "Compile executable..."
-g++ -std=c++23 ./src/prompt.cpp -o ./builds/prompt -Ofast -I./libs/ggerganov/whisper.cpp/include -I./libs/ggerganov/whisper.cpp/ggml/include ./libs/ggerganov/whisper.cpp/build/src/libwhisper.so.1.7.4 -Wl,-rpath,./libs/ggerganov/whisper.cpp/build/src -lrt -lm -lasound -lportaudio -lcurl -pthread
--- src/tools/_agency/agency.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Agency.hpp"
-#include "Agent.hpp"
-#include "Pack.hpp"
-#include "PackQueue.hpp"
